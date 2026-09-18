3884b95ee5d7caec599b5213b44a9c40e09f29c0
Marcus Martins <vini.mark2002@gmail.com>
2026-09-18 17:49:37 -0300

    perf(ai): parallelize monte carlo calculation with rayon for ~7.5x speedup

--- engine/src/ai.rs
+++ engine/src/ai.rs
@@ -1,3 +1,4 @@
+use rayon::prelude::*;
 use crate::card::{Card, Suit, Rank};
 use rand::seq::SliceRandom;
 
@@ -39,60 +40,61 @@ pub fn calculate_win_rate(
         }
     }
 
-    let mut wins = 0;
-    let mut ties = 0;
-    let mut rng = rand::rng(); // Using rand 0.10.2 syntax
+    let (wins, ties) = (0..iterations).into_par_iter().map_init(
+        || rand::rng(),
+        |rng, _| {
+            let mut sim_deck = full_deck.clone();
+            sim_deck.shuffle(rng);
 
-    for _ in 0..iterations {
-        let mut sim_deck = full_deck.clone();
-        sim_deck.shuffle(&mut rng);
+            // Deal remaining community cards
+            let needed_community = 5 - community_cards.len();
+            let mut sim_community = community_cards.to_vec();
+            for _ in 0..needed_community {
+                sim_community.push(sim_deck.pop().unwrap());
+            }
 
-        // Deal remaining community cards
-        let needed_community = 5 - community_cards.len();
-        let mut sim_community = community_cards.to_vec();
-        for _ in 0..needed_community {
-            sim_community.push(sim_deck.pop().unwrap());
-        }
+            // Evaluate my hand
+            let mut my_full = sim_community.clone();
+            my_full.extend_from_slice(my_cards);
+            
+            let my_rank = match crate::evaluator::evaluate(&my_full) {
+                Ok(r) => r,
+                Err(_) => return (0, 0),
+            };
 
-        // Evaluate my hand
-        let mut my_full = sim_community.clone();
-        my_full.extend_from_slice(my_cards);
-        
-        let my_rank = match crate::evaluator::evaluate(&my_full) {
-            Ok(r) => r,
-            Err(_) => continue,
-        };
+            // Deal opponents and evaluate
+            let mut won = true;
+            let mut tied = false;
 
-        // Deal opponents and evaluate
-        let mut won = true;
-        let mut tied = false;
+            for _ in 0..num_opponents {
+                let c1 = sim_deck.pop().unwrap();
+                let c2 = sim_deck.pop().unwrap();
+                let mut opp_full = sim_community.clone();
+                opp_full.push(c1);
+                opp_full.push(c2);
 
-        for _ in 0..num_opponents {
-            let c1 = sim_deck.pop().unwrap();
-            let c2 = sim_deck.pop().unwrap();
-            let mut opp_full = sim_community.clone();
-            opp_full.push(c1);
-            opp_full.push(c2);
-
-            if let Ok(opp_rank) = crate::evaluator::evaluate(&opp_full) {
-                if opp_rank > my_rank {
-                    won = false;
-                    tied = false;
-                    break;
-                } else if opp_rank == my_rank {
-                    tied = true;
+                if let Ok(opp_rank) = crate::evaluator::evaluate(&opp_full) {
+                    if opp_rank > my_rank {
+                        won = false;
+                        tied = false;
+                        break;
+                    } else if opp_rank == my_rank {
+                        tied = true;
+                    }
                 }
             }
-        }
 
-        if won {
-            if tied {
-                ties += 1;
+            if won {
+                if tied {
+                    (0, 1)
+                } else {
+                    (1, 0)
+                }
             } else {
-                wins += 1;
+                (0, 0)
             }
         }
-    }
+    ).reduce(|| (0, 0), |a, b| (a.0 + b.0, a.1 + b.1));
 
     // Ties can count as half a win for Expected Value
     (wins as f64 + (ties as f64 / 2.0)) / iterations as f64
